4f44ffc228ba198455bdf4db45a63d4ac5d8de2a
Matheus Battisti <matheusbudkewicz@gmail.com>
2019-03-21 23:37:55 -0300

    iniciando destructuring

---
+++ destructuring/scripts.js
@@ -0,0 +1,8 @@
+// array para variável
+const arr = [1,2,3,4,5];
+let [a,b,c,d,e] = arr;
+console.log(a); // 1
+console.log(b); // 2
+console.log(c); // 3
+console.log(d); // 4
+console.log(e); // 5
